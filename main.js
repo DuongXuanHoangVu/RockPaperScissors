5e0512b01c1bf98da1f846897d0bde81a36f1d22
Hoàng Vũ <121605731+DuongXuanHoangVu@users.noreply.github.com>
2023-02-06 16:59:53 +0700

    update ui

--- main.js
+++ main.js
@@ -1,76 +1,82 @@
-const playerChoices = {
-    ROCK: {
-        ROCK: 'tie',
-        PAPER: 'lose',
-        SCISSORS: 'win',
-    },
-    PAPER: {
-        PAPER: 'tie',
-        ROCK: 'win',
-        SCISSORS: 'lose',
-    },
-    SCISSORS: {
-        SCISSORS: 'tie',
-        ROCK: 'lose',
-        PAPER: 'win',
-    },
-}
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-function getComputerChoice () {
-    const computerChoice = Object.keys(playerChoices)[Math.floor(Math.random() * Object.keys(playerChoices).length)]
-    return computerChoice
-}
+const btns = $$(".button");
+const showResult = $(".show-result");
+const showGame = $(".show-game");
+const tieGame = $(".tie-game");
+const youWin = $('.player-win');
+const computerWin = $('.computer-win');
 
-function playerSelection () {
-    const playerChoice = prompt('Rock, paper, or scissors?').toUpperCase()
-    if (playerChoice === 'ROCK' || playerChoice === 'PAPER' || playerChoice ==='SCISSORS') {
-        return playerChoices[playerChoice]
-    } else {
-        alert('Please enter rock, paper, or scissors.')
-        return playerSelection()    
-    }
+const Choices = {
+  ROCK: {
+    ROCK: "tie",
+    PAPER: "lose",
+    SCISSORS: "win",
+  },
+  PAPER: {
+    PAPER: "tie",
+    ROCK: "win",
+    SCISSORS: "lose",
+  },
+  SCISSORS: {
+    SCISSORS: "tie",
+    ROCK: "lose",
+    PAPER: "win",
+  },
+};
+
+function getComputerChoice() {
+  const computerChoice =
+    Object.keys(Choices)[
+      Math.floor(Math.random() * Object.keys(Choices).length)
+    ];
+  return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    const result = {
-        isPlayerWin: false,
-        isComputerWin: false,
-    }
-    console.log(playerSelection)
-    console.log(computerSelection)
-    console.log(playerSelection[computerSelection])
-    if (playerSelection[computerSelection] === 'tie') {
-        alert('It\'s a tie!')
-    } else if (playerSelection[computerSelection] === 'win') {
-        result.isPlayerWin = true        
-        alert('You win!')
-    } else {
-        result.isComputerWin = true
-        alert('You lose!')
-    }
-    return result
+  const result = {
+    isPlayerWin: false,
+    isComputerWin: false,
+  };
+  if (Choices[playerSelection][computerSelection] === "tie") {
+    showResult.textContent = `It's a tie! Computer select ${computerSelection}`;
+  } else if (Choices[playerSelection][computerSelection] === "win") {
+    result.isPlayerWin = true;
+    showResult.textContent = `You Win! Computer select ${computerSelection}`;
+  } else {
+    result.isComputerWin = true;
+    showResult.textContent = `You Lose! Computer select ${computerSelection}`;
+  }
+  console.log(result.isPlayerWin);
+  return result;
 }
 
 function game() {
-    let countPlayerWin = 0;
-    let countComputerWin =0;
-    for (let i = 0; i < 5; i++) {
-        const result = playRound(playerSelection(), getComputerChoice())
-        if (result.isPlayerWin) {
-            countPlayerWin++
-        } else if (result.isComputerWin)  {
-            countComputerWin++
-        }
-        console.log(countPlayerWin)
-        console.log(countComputerWin)
-    }
-    if (countPlayerWin > countComputerWin) {
-        alert('Game over! You win!')
-    } else if (countPlayerWin < countComputerWin) {
-        alert('Game over! You lose!')
-    } else {
-        alert('Game over! tie!')
-    }
+  let count = 0;
+  let tie = 0;
+  let countPlayerWin = 0;
+  let countComputerWin = 0;
+  btns.forEach((btn) => {
+    btn.onclick = () => {
+      computerChoice = getComputerChoice();
+      playerChoice = btn.textContent.toUpperCase();
+      let result = playRound(playerChoice, computerChoice);
+      if (result.isPlayerWin) {
+        countPlayerWin++;
+      } else if (result.isComputerWin) {
+        countComputerWin++;
+      } else {
+        tie++
+      }
+      count++;
+      showGame.textContent = count;
+      tieGame.textContent = tie
+      youWin.textContent = countPlayerWin 
+      computerWin.textContent = countComputerWin
+    };
+  });
 }
 
-game()
+game();
+
